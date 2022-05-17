8ca3ae4e64037d5dbf06873564590a401cd3d581
Larry Liu <8188269+larryliu0820@users.noreply.github.com>
2022-05-16 21:25:50 -0700

    Add blog data

---
+++ app/src/main/java/com/example/http/BlogHttpClient.kt
@@ -0,0 +1,40 @@
+package com.example.http
+
+import android.util.Log
+import com.google.gson.Gson
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import java.util.concurrent.Executors
+
+object BlogHttpClient {
+    private const val BASE_URL = "https://raw.githubusercontent.com/larryliu0820/BlogApp/master/app/src/main/res/values/"
+    private const val BLOG_ARTICLES_URL = "blog_articles.json"
+
+    private val executor = Executors.newFixedThreadPool(4)
+    private val client = OkHttpClient()
+    private val gson = Gson()
+
+    fun loadBlogArticles(onSuccess: (List<Blog>) -> Unit, onError: () -> Unit) {
+        val request = Request.Builder().get()
+            .url(BASE_URL + BLOG_ARTICLES_URL)
+            .build()
+
+        executor.execute {
+            runCatching {
+                val response: Response = client.newCall(request).execute()
+                response.body?.string()?.let {
+                    json -> gson.fromJson(json, BlogData::class.java)?.let {
+                        blogData -> return@runCatching blogData.data
+                    }
+                }
+            }.onFailure { e: Throwable ->
+                Log.e("BlogHttpClient", "Error loading blog articles", e)
+                onError()
+            }.onSuccess { value: List<Blog>? ->
+                Log.d("BlogHttpClient", "Successfully loaded blog articles")
+                onSuccess(value ?: emptyList())
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/myapplication/BlogDetailsActivity.kt
+++ app/src/main/java/com/example/myapplication/BlogDetailsActivity.kt
@@ -5,33 +5,56 @@ import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CircleCrop
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.example.http.Blog
+import com.example.http.BlogHttpClient
 import com.example.myapplication.databinding.ActivityBlogDetailsBinding
 
-private const val IMAGE_URL = "https://bitbucket.org/dmytrodanylyk/travel-blog-resources/raw/" +
-        "3436e16367c8ec2312a0644bebd2694d484eb047/images/sydney_image.jpg"
-private const val AVATAR_URL = "https://bitbucket.org/dmytrodanylyk/travel-blog-resources/raw/" +
-        "3436e16367c8ec2312a0644bebd2694d484eb047/avatars/avatar1.jpg"
+private const val IMAGE_URL =
+    "https://raw.githubusercontent.com/larryliu0820/BlogApp/master/app/src/main/res/drawable/sydney_image.jpeg"
+private const val AVATAR_URL =
+    "https://raw.githubusercontent.com/larryliu0820/BlogApp/master/app/src/main/res/drawable/avatar_image.jpg"
 
 class BlogDetailsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityBlogDetailsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding = ActivityBlogDetailsBinding.inflate(layoutInflater)
+        binding = ActivityBlogDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        Glide.with(this).load(IMAGE_URL).into(binding.imageMain)
-        Glide.with(this).load(AVATAR_URL).transform(CircleCrop()).transition(
-            DrawableTransitionOptions.withCrossFade()).into(binding.imageAvatar)
 
-        binding.textTitle.text = "G'day from Sydney"
-        binding.textAuthor.text = "Grayson Wells"
-        binding.textDate.text = "August 2, 2019"
-        binding.textRating.text = "4.5"
-        binding.textViews.text = "(2687 views)"
-        binding.textDescription.text =
-            "Australia is one of the most popular travel destinations in the world."
-        binding.ratingBar.rating = 4.5f
         binding.imageBack.setOnClickListener {
             finish()
         }
+
+        loadData()
+    }
+
+    private fun loadData() {
+        BlogHttpClient.loadBlogArticles(onSuccess = { list: List<Blog> ->
+            runOnUiThread {
+                showData(list[0])
+            }
+        }, onError = {
+//            error: String -> runOnUiThread {
+//                showError(error)
+//            }
+        })
+    }
+
+    private fun showData(blog: Blog) {
+        binding.textTitle.text = blog.title
+        binding.textAuthor.text = blog.author.name
+        binding.textDate.text = blog.date
+        binding.textRating.text = blog.rating.toString()
+        binding.textViews.text = String.format("(%d views)", blog.views)
+        binding.textDescription.text = blog.description
+        binding.ratingBar.rating = blog.rating
+
+        Glide.with(this).load(blog.image).transition(DrawableTransitionOptions.withCrossFade())
+            .into(binding.imageMain)
+        Glide.with(this).load(blog.author.avatar).transform(CircleCrop()).transition(
+            DrawableTransitionOptions.withCrossFade()
+        ).into(binding.imageAvatar)
     }
 }
