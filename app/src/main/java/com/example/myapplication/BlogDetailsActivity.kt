70c108014755af72b73c653686c48e1324cdbbcf
Larry Liu <8188269+larryliu0820@users.noreply.github.com>
2022-05-16 22:09:27 -0700

    Add more content to blog

--- app/src/main/java/com/example/myapplication/BlogDetailsActivity.kt
+++ app/src/main/java/com/example/myapplication/BlogDetailsActivity.kt
@@ -1,6 +1,8 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.text.Html
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CircleCrop
@@ -8,6 +10,7 @@ import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.example.http.Blog
 import com.example.http.BlogHttpClient
 import com.example.myapplication.databinding.ActivityBlogDetailsBinding
+import com.google.android.material.snackbar.Snackbar
 
 private const val IMAGE_URL =
     "https://raw.githubusercontent.com/larryliu0820/BlogApp/master/app/src/main/res/drawable/sydney_image.jpeg"
@@ -36,19 +39,30 @@ class BlogDetailsActivity : AppCompatActivity() {
                 showData(list[0])
             }
         }, onError = {
-//            error: String -> runOnUiThread {
-//                showError(error)
-//            }
+            runOnUiThread {
+                showErrorSnackbar()
+            }
         })
     }
 
+    private fun showErrorSnackbar() {
+        Snackbar.make(binding.root, "Error loading blog details", Snackbar.LENGTH_INDEFINITE).run {
+            setActionTextColor(resources.getColor(R.color.orange500))
+            setAction("Retry") {
+                loadData()
+                dismiss()
+            }
+        }.show()
+    }
+
     private fun showData(blog: Blog) {
+        binding.progressBar.visibility = View.GONE
         binding.textTitle.text = blog.title
         binding.textAuthor.text = blog.author.name
         binding.textDate.text = blog.date
         binding.textRating.text = blog.rating.toString()
         binding.textViews.text = String.format("(%d views)", blog.views)
-        binding.textDescription.text = blog.description
+        binding.textDescription.text = Html.fromHtml(blog.description)
         binding.ratingBar.rating = blog.rating
 
         Glide.with(this).load(blog.image).transition(DrawableTransitionOptions.withCrossFade())
